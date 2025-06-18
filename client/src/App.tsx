3da93198079673c5a5e54c7e24ba0a7e3fd8f96a
tomasbaublys <t.baublys@icloud.com>
2025-06-18 18:09:59 +0300

    Created basic components structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
-const App = () => {
+import { Route, Routes } from "react-router";
+import MainOutlet from "./components/outlets/MainOutlet";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
 
+
+const App = () => {
   return (
     <>
-      
+      <Routes>
+        <Route path="" element={<MainOutlet />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
     </>
-  )
-}
+  );
+};
 
 export default App;
